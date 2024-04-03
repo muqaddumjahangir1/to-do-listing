c68f3e35326744ae3697f9295be9652d67f4eb12
MUQADDUM JAHANGIR <164214566+muqaddumjahangir1@users.noreply.github.com>
2024-04-03 19:38:28 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,37 @@
+import inquirer from "inquirer"; 
+
+//user variable
+let todos=[];
+while(true){
+    let input = await inquirer.prompt([
+        
+{
+  
+    name:"Todo item",
+    type:"input",
+    message:'add todo item in listing'
+},
+
+{
+    name:"addMore",
+    type:'list',
+    choice: ["yes,'no"]
+}
+]);
+
+// output of program
+const{Todoitem,addMore}=input;
+
+todos.push(Todoitem);
+if (addMore === "no"){
+    console.log("Todolist:");
+
+    //add more item
+for(let i=0;i<todos.length; i++)
+{console.log(todos[i])} 
+
+//exit loop
+break;
+
+}
+}
